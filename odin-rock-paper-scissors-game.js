0c0916c041aeadc49d9a1fbaa3db6166dac14597
dossonang <179678372+DossonAng@users.noreply.github.com>
2025-07-11 15:19:41 +0800

    Change the values of the "randomNumber" variable
    Change the values of the "randomNumber" variable in "getComputerChoice"
    function from decimals to fractions to make it clearer and also for the
    last third to be an exact.

--- odin-rock-paper-scissors-game.js
+++ odin-rock-paper-scissors-game.js
@@ -1,9 +1,9 @@
 function getComputerChoice() {
   let randomNumber = Math.random();
 
-  if (randomNumber <= 0.33) {
+  if (randomNumber < 1 / 3) {
     return "rock";
-  } else if (randomNumber <=0.66) {
+  } else if (randomNumber < 2 / 3) {
     return "paper";
   } else {
     return "scissors";
