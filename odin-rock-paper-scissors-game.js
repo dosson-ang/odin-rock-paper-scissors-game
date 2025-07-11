754665732cad5c0f24ab109a0aecacaddb08c5e4
dossonang <179678372+DossonAng@users.noreply.github.com>
2025-07-11 15:06:29 +0800

    Create "playRound" function
    Create a function named "playRound" that will run a single round of the
    Rock, Paper, Scissors game where it will increment the point of the
    winner between the Player and the Computer depending on what they choose
    to play.

--- odin-rock-paper-scissors-game.js
+++ odin-rock-paper-scissors-game.js
@@ -25,3 +25,31 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === "rock" && computerChoice === "scissors") {
+    console.log("You win! Rock beats Scissors.");
+    humanScore++;
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    console.log("You win! Paper beats Rock.");
+    humanScore++;
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    console.log("You win! Scissors beats Paper.");
+    humanScore++;
+  } else if (humanChoice === "rock" && computerChoice === "paper") {
+    console.log("You lose! Paper beats Rock.");
+    computerScore++;
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    console.log("You lose! Scissors beats Paper.");
+    computerScore++;
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    console.log("You lose! Rock beats Scissors.");
+    computerScore++;
+  } else if (humanChoice === "rock" && computerChoice === "rock") {
+    console.log("Rock against Rock is a draw!");
+  } else if (humanChoice === "paper" && computerChoice === "paper") {
+    console.log("Paper against Paper is a draw!");
+  } else if (humanChoice === "scissors" && computerChoice === "scissors") {
+    console.log("Scissors against Scissors is a draw!");
+  } 
+}
