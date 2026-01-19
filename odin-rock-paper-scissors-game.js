f87665034c0b2524655ef80da19e5c58d4619211
dossonang <179678372+DossonAng@users.noreply.github.com>
2026-01-19 08:03:19 +0800

    Remove playGame() logic & getHumanChoice() func
    Remove the playGame() logic and the getHumanChoice() function to
    prepare for rewriting the code, changing it from the player entering
    their choice on a prompt, to the player selecting their choice from one
    of three buttons.

--- odin-rock-paper-scissors-game.js
+++ odin-rock-paper-scissors-game.js
@@ -10,58 +10,33 @@ function getComputerChoice() {
   }
 }
 
-function getHumanChoice() {
-  let humanAnswer = prompt('Type in either "rock", "paper", or "scissors":');
-  let humanAnswerInLowerCase = humanAnswer.toLowerCase();
+let humanScore = 0;
+let computerScore = 0;
 
-  if (humanAnswerInLowerCase === 'rock') {
-    return 'rock';
-  } else if (humanAnswerInLowerCase === 'paper') {
-    return 'paper';
-  } else if (humanAnswerInLowerCase === 'scissors') {
-    return 'scissors';
-  }
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === 'rock' && computerChoice === 'scissors') {
+    console.log('You win! Rock beats Scissors.');
+    humanScore++;
+  } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+    console.log('You win! Paper beats Rock.');
+    humanScore++;
+  } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+    console.log('You win! Scissors beats Paper.');
+    humanScore++;
+  } else if (humanChoice === 'rock' && computerChoice === 'paper') {
+    console.log('You lose! Paper beats Rock.');
+    computerScore++;
+  } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+    console.log('You lose! Scissors beats Paper.');
+    computerScore++;
+  } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+    console.log('You lose! Rock beats Scissors.');
+    computerScore++;
+  } else if (humanChoice === 'rock' && computerChoice === 'rock') {
+    console.log('Rock against Rock is a draw!');
+  } else if (humanChoice === 'paper' && computerChoice === 'paper') {
+    console.log('Paper against Paper is a draw!');
+  } else if (humanChoice === "scissors" && computerChoice === 'scissors') {
+    console.log('Scissors against Scissors is a draw!');
+  } 
 }
-
-function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
-
-  function playRound(humanChoice, computerChoice) {
-    if (humanChoice === 'rock' && computerChoice === 'scissors') {
-      console.log('You win! Rock beats Scissors.');
-      humanScore++;
-    } else if (humanChoice === 'paper' && computerChoice === 'rock') {
-      console.log('You win! Paper beats Rock.');
-      humanScore++;
-    } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
-      console.log('You win! Scissors beats Paper.');
-      humanScore++;
-    } else if (humanChoice === 'rock' && computerChoice === 'paper') {
-      console.log('You lose! Paper beats Rock.');
-      computerScore++;
-    } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
-      console.log('You lose! Scissors beats Paper.');
-      computerScore++;
-    } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
-      console.log('You lose! Rock beats Scissors.');
-      computerScore++;
-    } else if (humanChoice === 'rock' && computerChoice === 'rock') {
-      console.log('Rock against Rock is a draw!');
-    } else if (humanChoice === 'paper' && computerChoice === 'paper') {
-      console.log('Paper against Paper is a draw!');
-    } else if (humanChoice === "scissors" && computerChoice === 'scissors') {
-      console.log('Scissors against Scissors is a draw!');
-    } 
-  }
-
-  if (humanScore > computerScore) {
-    console.log(`The final standings are ${humanScore} - ${computerScore}! Congrats! You win the game!`);
-  } else if (humanScore < computerScore) {
-    console.log(`The final standings are ${humanScore} - ${computerScore}! The computer wins the game! Better luck next time!`);
-  } else {
-    console.log(`The final standings are ${humanScore} - ${computerScore}! It's a tie!`);
-  }
-}
-
-playGame();
