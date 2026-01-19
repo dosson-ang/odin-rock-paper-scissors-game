6c897cba96f5228e6018554385c64d2d2713a423
dossonang <179678372+DossonAng@users.noreply.github.com>
2026-01-19 10:55:39 +0800

    Show final results once someone reaches 5 points
    Create a div element with an id attribute of "finalResults" as well as
    a variable with the same name that will display the overall winner of
    the RPS game once either the Player or the Computer reaches a score of 5
    points.
    
    Create an endGame() function that will disable the 3 RPS buttons once
    either the Player or the Computer reaches a score of 5 points.
    
    Create a checkGameOver() function that will check after every round if
    either the Player or Computer has reached 5 points.

--- odin-rock-paper-scissors-game.js
+++ odin-rock-paper-scissors-game.js
@@ -13,30 +13,53 @@ function getComputerChoice() {
 let humanScore = 0;
 let computerScore = 0;
 let roundResults = document.querySelector("#roundResults");
+let finalResults = document.querySelector("#finalResults");
+
+function endGame() {
+  rockButton.disabled = true;
+  paperButton.disabled = true;
+  scissorsButton.disabled = true;
+}
+
+function checkGameOver() {
+  if (humanScore === 5) {
+    finalResults.textContent = 'You have reached 5 points! You win the game!';
+    endGame();
+  } else if (computerScore === 5) {
+    finalResults.textContent = 'The computer has reached 5 points! You lose the game!';
+    endGame();
+  }
+}
 
 function playRound(humanChoice, computerChoice) {
   if (humanChoice === 'rock' && computerChoice === 'scissors') {
     humanScore++;
     roundResults.textContent = `You win! Rock beats Scissors. Scores: You - ${humanScore} Computer - ${computerScore}`;
+    checkGameOver();
   } else if (humanChoice === 'rock' && computerChoice === 'paper') {
     computerScore++;
     roundResults.textContent = `You lose! Paper beats Rock. Scores: You - ${humanScore} Computer - ${computerScore}`;
+    checkGameOver();
   } else if (humanChoice === 'rock' && computerChoice === 'rock') {
     roundResults.textContent = `Rock against Rock is a draw! Scores: You - ${humanScore} Computer - ${computerScore}`;
   } else if (humanChoice === 'paper' && computerChoice === 'rock') {
     humanScore++;
     roundResults.textContent = `You win! Paper beats Rock. Scores: You - ${humanScore} Computer - ${computerScore}`;
+    checkGameOver();
   } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
     computerScore++;
     roundResults.textContent = `You lose! Scissors beats Paper. Scores: You - ${humanScore} Computer - ${computerScore}`;
+    checkGameOver();
   } else if (humanChoice === 'paper' && computerChoice === 'paper') {
     roundResults.textContent = `Paper against Paper is a draw! Scores: You - ${humanScore} Computer - ${computerScore}`;
   } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
     humanScore++;
     roundResults.textContent = `You win! Scissors beats Paper. Scores: You - ${humanScore} Computer - ${computerScore}`;
+    checkGameOver();
   } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
     computerScore++;
     roundResults.textContent = `You lose! Rock beats Scissors. Scores: You - ${humanScore} Computer - ${computerScore}`;
+    checkGameOver();
   } else if (humanChoice === "scissors" && computerChoice === 'scissors') {
     roundResults.textContent = `Scissors against Scissors is a draw! Scores: You - ${humanScore} Computer - ${computerScore}`;
   } 
